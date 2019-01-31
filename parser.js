4693d061d0dd53b924506108e77f2e3acedbed0d
Matheus Clark dos Santos Monte <matheus.m@samsung.com>
2019-01-31 15:14:36 -0400

    Parser data to get only job name and salary average
    Signed-off-by: Matheus Clark dos Santos Monte <matheus.m@samsung.com>

--- parser.js
+++ parser.js
@@ -6,8 +6,9 @@ function extractSalaryFromHTML(html){
   //console.log('Estou', jobsRow);
   const salaryPerJob = [];
   jobsRow.each((i, el) => {
-    let job = $(el).children('td').text();
-    salaryPerJob.push(job);
+    let job = $(el).children('td').slice(1).eq(0).text();
+    let salary = $(el).children('td').slice(4).eq(0).text();
+    salaryPerJob.push({job, salary});
   }); 
   return salaryPerJob;  
 };
